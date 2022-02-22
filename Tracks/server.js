5eeccb80ef5dc5667ca0845f86574591f4c58d5b
ChescaRobertson <francescamarierobertson@hotmail.co.uk@>
2022-02-22 14:37:37 +0000

    Exercise 7 Express framework added to Tracks

---
+++ Tracks/server.js
@@ -0,0 +1,37 @@
+let express = require('express');
+let app = express();
+let tracks = require('./tracks');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '//tracks.html');
+});
+
+app.get('/getTrack', (req, res) => {
+  let responseTrack = tracks.getTrack(req.query.title);
+  res.send(JSON.stringify(responseTrack));
+});
+
+app.get('/remove', (req, res) => {
+  let title = req.query.title;
+  tracks.removeTrack(title);
+  res.send(`<h2>Removed track ${title}<h2>`);
+});
+
+app.get('/list', (req, res) => {
+  res.send(JSON.stringify(tracks.listTracks()));
+});
+
+app.post('/addTrack', (req, res) => {
+  tracks.addTrack(req.body.title, req.body.artist, req.body.album);
+  let responseTrack = {
+    title: req.body.title,
+    artist: req.body.artist,
+    album: req.body.album,
+  };
+  res.send(JSON.stringify(responseTrack));
+});
+
+app.listen(8081);
